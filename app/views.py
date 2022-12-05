be332b0f1e0fe8bdfd0c1345860fc5c3beafb993
Sourcery AI <>
2022-12-05 02:26:40 +0000

    'Refactored by Sourcery'

--- app/views.py
+++ app/views.py
@@ -29,9 +29,7 @@ def post_vote():
         voter = VotesModel(roll_num=current_user.roll_num,voter_id=current_user.id,post_1=int(president),post_2=int(vicepresident))
         db.session.add(voter)
         db.session.commit()
-        return redirect(url_for('profile'))
-    else:
-        return redirect(url_for('profile'))
+    return redirect(url_for('profile'))
     
 
 @app.route("/candidate")
@@ -43,12 +41,11 @@ def candidate():
 @app.route("/candidate_register")
 @login_required
 def candidate_register():
-    if current_user.admin !=1:
-        logout_user()
-        flash('You do not have required authorization')
-        return redirect(url_for('auth.login'))
-    else:
+    if current_user.admin == 1:
         return render_template("candidate_register.html")
+    logout_user()
+    flash('You do not have required authorization')
+    return redirect(url_for('auth.login'))
 
 @app.route("/candidate_register", methods=["POST"])
 def candidate_post():
@@ -61,7 +58,7 @@ def candidate_post():
     post = request.form.get('post')
     pic_path = request.form.get('pic_path')
     agenda = request.form.get('agenda')
-    
+
     roll_no = UserModel.query.filter_by(roll_num =roll_num).first()
     cand = CandidateModel.query.filter_by(roll_num = roll_num).first()
 
@@ -74,12 +71,12 @@ def candidate_post():
     if cand:
         flash('Candidate has already been registered.','error')
         return redirect(url_for('candidate_register'))
-    
-    if not set(first_name).issubset(string.ascii_letters + " "):
+
+    if not set(first_name).issubset(f"{string.ascii_letters} "):
         flash('Name can only contain alphabets.','error')
         error = True
-    
-    if not set(last_name).issubset(string.ascii_letters + " "):
+
+    if not set(last_name).issubset(f"{string.ascii_letters} "):
         flash('Name can only contain alphabets.','error')
         error = True
 
@@ -90,15 +87,13 @@ def candidate_post():
     if not batch and not course and not department:
         flash('Please fill in all the details. Batch, Course and Department information is neccessary.','error')
         error = True
-    
-    if error:
-        return redirect(url_for('candidate_register'))
-    else:
+
+    if not error:
         candidate = CandidateModel(roll_num=roll_num, first_name=first_name,last_name=last_name,batch=batch,course=course,department=department,post=post,pic_path=pic_path,agenda=agenda)
         db.session.add(candidate)
         db.session.commit()
         flash('Candidate successfully registered.','success')
-        return redirect(url_for('candidate_register'))
+    return redirect(url_for('candidate_register'))
 
 @app.route("/live_result")
 def live_result():
@@ -109,16 +104,16 @@ def live_result():
     labels1=[]
     data1=[]
     for candidate in prez:
-        name = candidate.first_name+" "+candidate.last_name
+        name = f"{candidate.first_name} {candidate.last_name}"
         labels.append(name)
         vote=VotesModel.query.filter(VotesModel.post_1==candidate.roll_num).count()
         data.append(vote)
     for candidate in vice:
-        name = candidate.first_name+" "+candidate.last_name
+        name = f"{candidate.first_name} {candidate.last_name}"
         labels1.append(name)
         vote=VotesModel.query.filter(VotesModel.post_2==candidate.roll_num).count()
         data1.append(vote)
- 
+
     return render_template('graph.html',labels=labels,data=data,labels1=labels1,data1=data1)
 
 
@@ -132,12 +127,12 @@ def voteCount():
     labels1=[]
     data1=[]
     for candidate in prez:
-        name = candidate.first_name+" "+candidate.last_name
+        name = f"{candidate.first_name} {candidate.last_name}"
         labels.append(name)
         vote=VotesModel.query.filter(VotesModel.post_1==candidate.roll_num).count()
         data.append(vote)
     for candidate in vice:
-        name = candidate.first_name+" "+candidate.last_name
+        name = f"{candidate.first_name} {candidate.last_name}"
         labels1.append(name)
         vote=VotesModel.query.filter(VotesModel.post_2==candidate.roll_num).count()
         data1.append(vote)
@@ -146,9 +141,6 @@ def voteCount():
             "labels": labels,
             "data1": data1,
             "labels1": labels1}
-    response = app.response_class(
-        response=json.dumps(output),
-        status=200,
-        mimetype='application/json'
+    return app.response_class(
+        response=json.dumps(output), status=200, mimetype='application/json'
     )
-    return response
